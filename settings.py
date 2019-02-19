ab3fb4e4be26abc61537d2b5bf751fd1efb21a97
PeterTao666 <taofeng1993@163.com>
2019-02-19 23:36:32 +0800

    2.19 击落飞船

--- settings.py
+++ settings.py
@@ -9,11 +9,18 @@ class Settings():
         self.bg_color = (230, 230, 230)
 
         # 飞船的设置
-        self.ship_speed_factor = 0.5
+        self.ship_speed_factor = 1
+        self.ship_limit = 3
 
         # 子弹设置
-        self.bullet_speed_factor = 0.2
+        self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 5
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 5
+
+        # 外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 5
+        # fleet_direction 为1表示向右移动，为-1表示左移
+        self.fleet_direction = 1
